1faef571fa09a3cdd89bb80796aa521811abe53b
Strobes27 <Strobes27@users.noreply.github.com>
2015-05-21 11:36:39 +0200

    hackerrank-tweets
    Solves all test cases

---
+++ hackerrank-tweets.py
@@ -0,0 +1,17 @@
+
+#Solution to https://www.hackerrank.com/challenges/detect-the-email-addresses
+
+# by: Strobes27
+
+import re
+
+N = int(raw_input())
+count = 0
+
+for _ in range(N):
+    tweet = raw_input()
+    match = re.search(r'hackerrank', tweet, re.IGNORECASE)
+    if match:
+        count += 1
+        
+print count
